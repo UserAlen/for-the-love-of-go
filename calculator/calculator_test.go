c5940997cce08a8fc16f7a622a1a7f45712a86be
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-01-28 18:43:12 +0300

    added substract func

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -8,12 +8,33 @@ import (
 func TestAdd(t *testing.T) {
 	t.Parallel()
 
-	got := calculator.Add(2, 2)
+	var a, b float64
+
+	a, b = 2, 2
+
+	got := calculator.Add(a, b)
 
 	//it's needed to be initialized explicitly otherwise "want := 4" is int
 	var want float64 = 4
 
 	if got != want {
-		t.Errorf("got %f want %f", got, want)
+		t.Errorf("Add(%.1f, %.1f): got %.1f want %.1f", a, b, got, want)
 	}
+}
+
+func TestSubstract(t *testing.T) {
+	t.Parallel()
+
+	var a, b float64
+
+	a = 4
+	b = 2
+
+	got := calculator.Substract(a, b)
+	var want float64 = 2
+
+	if got != want {
+		t.Errorf("Substract(%.1f, %.1f): got %.1f want %.1f", a, b, got, want)
+	}
+
 }
