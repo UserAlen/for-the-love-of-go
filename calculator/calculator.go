e8ea63d0fc55db898bba8c32710f7ce56dca9752
UserAlen <93518707+UserAlen@users.noreply.github.com>
2024-01-29 19:51:02 +0300

    added test cases + other operations

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -7,3 +7,11 @@ func Add(a, b float64) float64 {
 func Subtract(a, b float64) float64 {
 	return a - b
 }
+
+func Multiply(a, b float64) float64 {
+	return a * b
+}
+
+func Divide(a, b float64) float64 {
+	return a / b
+}
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -38,3 +38,52 @@ func TestSubtract(t *testing.T) {
 	}
 
 }
+
+func TestMultiply(t *testing.T) {
+
+	t.Parallel()
+
+	type testCases struct {
+		a, b, want float64
+	}
+
+	tcs := []testCases{
+		{a: 5, b: 75, want: 375},
+		{a: 6, b: 348, want: 2088},
+		{a: 7, b: 89, want: 623},
+		{a: 12, b: 574, want: 6888},
+		{a: 11, b: 53549, want: 589039},
+	}
+
+	for _, tc := range tcs {
+		got := calculator.Multiply(tc.a, tc.b)
+
+		if got != tc.want {
+			t.Errorf("Multiply(%.1f, %.1f): got %.1f want %.1f", tc.a, tc.b, got, tc.want)
+		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a, b, want float64
+	}
+
+	tcs := []testCase{
+		{a: 5, b: 2, want: 2.5},
+		{a: 6, b: 3, want: 2},
+		{a: 75, b: 5, want: 15}, //always put trailing comma
+		{a: 8, b: 2, want: 8 / 2},
+	}
+
+	for _, tc := range tcs {
+		got := calculator.Divide(tc.a, tc.b)
+
+		if got != tc.want {
+			t.Errorf("Divide(%f, %f): want %f got %f", tc.a, tc.b, got, tc.want)
+		}
+	}
+
+}
